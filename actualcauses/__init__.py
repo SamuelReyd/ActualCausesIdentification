be27b58339ebb4e6af5e2aaa3c58ba9cdd74a8b5
Samuel Reyd-Fajgeles <sreyd@nodecpu10.enst.fr>
2026-01-20 17:24:09 +0100

    restructuring

--- actualcauses/__init__.py
+++
@@ -1,7 +0,0 @@
-from .isi import iterative_identification
-from .mbs import beam_search
-from .lucb import lucb
-from .system_model import SystemModel, BaseNumpyModel, AverageNumpyModel, LUCBNumpyModel
-
-__all__ = ['beam_search', 'iterative_identification', 'lucb', 'SystemModel', 'BaseNumpyModel', 'AverageNumpyModel', 'LUCBNumpyModel']
-__version__ = "1.0.0"
